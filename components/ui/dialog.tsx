c9ed808b313ea9f8ad0b224947426cd6df6a1943
Loki <loki2723@mail.ru>
2026-01-02 00:16:02 +0300

    fix: СѓСЃС‚СЂР°РЅРµРЅРѕ РјРµСЂС†Р°РЅРёРµ РјРѕРґР°Р»СЊРЅС‹С… РѕРєРѕРЅ РїРѕРґРїРёСЃРєРё РЅР° РјРѕР±РёР»СЊРЅС‹С…

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -140,11 +140,11 @@ function DialogContent({
 }) {
   return (
     <DialogPortal data-slot="dialog-portal">
-      <DialogOverlay className="backdrop-blur-sm z-[45]" />
+      <DialogOverlay className="z-[45]" />
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-[60] grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 outline-none sm:max-w-lg",
+          "bg-background fixed top-[50%] left-[50%] z-[60] grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg outline-none sm:max-w-lg",
           className
         )}
         {...props}
