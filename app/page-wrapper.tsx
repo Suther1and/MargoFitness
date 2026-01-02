625cd0fe2992b4b81582c09df7bdc59baa57aa84
Loki <loki2723@mail.ru>
2026-01-02 20:25:55 +0300

    Fix build error: remove duplicate page-wrapper.tsx

--- app/page-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { getCurrentProfile } from '@/lib/actions/profile'
-import HomeNewPage from './page'
-
-export default async function HomePageWrapper() {
-  const profile = await getCurrentProfile()
-  
-  return <HomeNewPage initialProfile={profile} />
-}
-
